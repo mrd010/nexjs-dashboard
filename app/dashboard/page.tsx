68f3bd5eea108a74b4d74d71b04e1fc1a0b81d91
DarkVergil <126393669+mrd010@users.noreply.github.com>
2024-05-16 12:02:52 +0330

    Add revenu chart and latest invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Card } from '../ui/dashboard/cards';
+import { lusitana } from '../ui/fonts';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { fetchLatestInvoices, fetchRevenue } from '../lib/data';
 
-const page = () => {
-  return <div>Dashboard Page</div>;
+const page = async () => {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* <Card title="Collected" value={} type="collected"></Card>
+        <Card title="Pending" value={} type="pending"></Card>
+        <Card title="Total Invoices" value={} type="invoices"></Card>
+        <Card title="Total Customers" value={} type="customers"></Card> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8 ">
+        <RevenueChart revenue={revenue}></RevenueChart>
+        <LatestInvoices latestInvoices={latestInvoices}></LatestInvoices>
+      </div>
+    </main>
+  );
 };
 
 export default page;
