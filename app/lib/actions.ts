397dfbbcbf7fff36ff6a0d26cbd3475c40e64e5f
DarkVergil <126393669+mrd010@users.noreply.github.com>
2024-05-21 19:40:02 +0330

    Finish project

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
+import { signIn } from '@/auth';
 import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
@@ -112,5 +114,25 @@ export const deleteInvoice = async (id: string) => {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+};
+
+export const authenticate = async (
+  prevState: string | undefined,
+  formData: FormData,
+) => {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 };
